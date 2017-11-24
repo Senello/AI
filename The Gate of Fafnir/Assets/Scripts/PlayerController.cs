0c9b4e034ce5b59f8e5474016fa469a2f85a7720
Senello <matoz1995@gmail.com>
2017-11-24 21:11:34 +0100

    pierwsza wersja

---
+++ The Gate of Fafnir/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed;
+    public float smoothTime = 0.3F;
+    public Boundary boundary;
+    
+    private Transform target;
+    private Vector3 velocity = Vector3.zero;
+
+    [System.Serializable]
+    public class Boundary
+    {
+        public float xMin, xMax, zMin, zMax;
+    }
+    void Start()
+    {
+        target = GetComponent<Transform>();
+    }
+
+
+    void Update()
+    {
+        float xSpeed = 0;
+        float ySpeed = 0;
+        float zSpeed = 0;
+        if (Input.GetKey(KeyCode.W)) zSpeed = playerSpeed;
+        if (Input.GetKey(KeyCode.S)) zSpeed = -playerSpeed;
+        if (Input.GetKey(KeyCode.A)) xSpeed = -playerSpeed;
+        if (Input.GetKey(KeyCode.D)) xSpeed = playerSpeed;
+        Vector3 targetPosition = target.TransformPoint(new Vector3(xSpeed, ySpeed, zSpeed));
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        transform.position = new Vector3
+        (
+            Mathf.Clamp(transform.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(transform.position.z, boundary.zMin, boundary.zMax)
+        );
+    }
+}
