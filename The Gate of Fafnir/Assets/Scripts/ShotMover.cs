ca2c8dd2f2e9c3156c8db5f014fdaaa328edee43
Senello <matoz1995@gmail.com>
2017-11-25 17:25:27 +0100

    nowe kolory i particle

--- The Gate of Fafnir/Assets/Scripts/ShotMover.cs
+++ The Gate of Fafnir/Assets/Scripts/ShotMover.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class ShotMover : MonoBehaviour
 {
-    public float speed;
+    public GameObject Explosion;
+    public GameObject Shooter;
+    public float Speed;
+    public float LifeTime;
     private Rigidbody rb;
+    
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.velocity = transform.up * speed;
+        rb.velocity = transform.up * Speed;
+        Destroy(gameObject, LifeTime);
     }
 
     void OnTriggerEnter(Collider col)
     {
-        Destroy(gameObject);
+        if (col.name != Shooter.name)
+        {
+            Instantiate(Explosion, gameObject.transform.position, gameObject.transform.rotation);
+            Destroy(gameObject);
+        }
     }
 }
