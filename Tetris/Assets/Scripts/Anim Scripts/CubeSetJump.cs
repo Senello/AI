f5c01b6cebd5898ba84fefd87ffe2abbf43462e1
Senello <matoz1995@gmail.com>
2017-12-10 23:52:47 +0100

    New Animations

---
+++ Tetris/Assets/Scripts/Anim Scripts/CubeSetJump.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSetJump : StateMachineBehaviour
+{
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(GameController.Instace.GetShake()) animator.SetTrigger("Jump");
+    }
+}
